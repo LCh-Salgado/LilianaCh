cff162af6197393762d93556195713d84e47c6dd
Chicas-Salgado <1335617@bcpss.org>
2024-10-01 14:07:42 -0400

    added Question class

---
+++ app/src/main/java/com/example/piquizapplch/Question.java
@@ -0,0 +1,39 @@
+package com.example.piquizapplch;
+
+public class Question {
+
+    private String qText;
+    private  boolean correctAns;
+
+    public  Question() {
+        qText = "unknow";
+        correctAns = true;
+    }
+
+    public Question(String sQText, boolean sCorrectAns) {
+        qText = sQText;
+        correctAns = sCorrectAns;
+    }
+
+    public boolean getCorrAns() {
+        return correctAns;
+    }
+
+    public void  setQText(String newQT) {
+        qText = newQT;
+    }
+
+    public String getQText() {
+        return qText;
+    }
+
+    public void setCorrect(boolean newA) {
+        correctAns = newA;
+    }
+
+    @Override
+    public String toString() {
+        return "question Text: " + qText +
+            "correctAnswers" +  correctAns;
+    }
+}
