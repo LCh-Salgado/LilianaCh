f6e902b890ec03ba77ab1e203116fa6c53b92701
Chicas-Salgado <1335617@bcpss.org>
2024-10-01 14:48:05 -0400

    added 10 Questions

--- app/src/main/java/com/example/piquizapplch/MainActivity.java
+++ app/src/main/java/com/example/piquizapplch/MainActivity.java
@@ -16,11 +16,14 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     TextView questionTV;
-    Button trueBTN, falseBTN, doneBTN;
+    Button trueBTN, falseBTN, nextBTN;
     int score, duration;
     String toastMsg;
     Toast myToast;
     Intent goToSA;
+    Question q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, currentQ;
+    Question[] questions;
+    int currentIndex;
 
 
     @Override
@@ -30,10 +33,24 @@ public class MainActivity extends AppCompatActivity {
 
         score = 0;
         toastMsg ="You got the question wrong.";
+        q1 = new Question("The Witcher 3 Wild Hunt was developed by Ubisoft?", false);
+        q2 = new Question("The original Super Mario Bross Game was released in 1985?", true);
+        q3 = new Question("The game Minecraft was first released in 2011?", true);
+        q4 = new Question("There are more than 15 games in the Call Of Duty Series?", false);
+        q5 = new Question("The Nintendo Switch was released in 2016?", true);
+        q6 = new Question("Pac-Man was originally called Puck Man?", true);
+        q7 = new Question("The game Fornite was originally developed as a zombie survival game before the battle royal mode was introduced?",true);
+        q8 = new Question("The Pokemon Franchise was created by Sega?", false);
+        q9 = new Question("The game Among Us became ")
+        q10 = new Question()
+
+        currentIndex = 0;
+        currentQ = q1;
+        questions = new Question[] {q1, q2, q3, q4, q5, q6, q7, q8, q9, q10};
         questionTV = findViewById(R.id.questionTV);
         trueBTN = findViewById(R.id.trueBTN);
         falseBTN = findViewById(R.id.falseBTN);
-        doneBTN = findViewById(R.id.doneBTN);
+        nextBTN = findViewById(R.id.nextBTN);
 
         trueBTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,12 +75,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        doneBTN.setOnClickListener(new View.OnClickListener() {
+        nextBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToSA = new Intent(MainActivity.this, ScoreActivity.class);
-                goToSA.putExtra("Score", score);
-                startActivity(goToSA);
+                if (currentIndex == 9) {
+                    goToSA = new Intent(MainActivity.this, ScoreActivity.class);
+                    goToSA.putExtra("Score", score);
+                    startActivity(goToSA);
+
+                }
+                else {
+                    currentIndex++;
+                    currentQ = questions[currentIndex];
+                    questionTV.setText(currentQ.getQText());
+
+                }
+
             }
         });
 
