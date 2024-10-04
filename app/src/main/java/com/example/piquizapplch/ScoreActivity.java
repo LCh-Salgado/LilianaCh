9839e072d64f0b7479bef16144da84d2bb7604e8
Chicas-Salgado <1335617@bcpss.org>
2024-10-04 14:57:59 -0400

    added changes

--- app/src/main/java/com/example/piquizapplch/ScoreActivity.java
+++ app/src/main/java/com/example/piquizapplch/ScoreActivity.java
@@ -1,7 +1,10 @@
 package com.example.piquizapplch;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,17 +18,42 @@ public class ScoreActivity extends AppCompatActivity {
     TextView scoreTV;
     int score;
     Intent welcomeToSA;
+    Button sendScoreByE;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        sendScoreByE = (Button)findViewById(R.id.sendE);
 
         scoreTV = findViewById(R.id.scoreTV);
         score = 0;
         welcomeToSA = getIntent();
         score = welcomeToSA.getIntExtra("Score", 0);
         scoreTV.setText("" + score);
+
+        sendScoreByE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] addresses = new String[] {"lilianachicas180@gmail.com"};
+                String subject = "New score on the video games app";
+                String body = "Score of your quiz: " + score;
+                composeEmail(addresses, subject, body);
+
+
+            }
+        });
+    }
+
+    private void composeEmail(String[] addresses, String subject, String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // Only email apps handle this.
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/piquizapplch/WelcomeActivity.java
+++ app/src/main/java/com/example/piquizapplch/WelcomeActivity.java
@@ -22,6 +22,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        goBTN = (Button)findViewById(R.id.touchBTN);
 
         goBTN.setOnClickListener(new View.OnClickListener() {
             @Override
