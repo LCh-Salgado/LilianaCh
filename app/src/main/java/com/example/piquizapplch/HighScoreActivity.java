0a48db1ba3a99d3a532f8aa8302eedbfc88273f2
LCh-Salgado <lilianachicas180@gmail.com>
2024-12-17 11:50:08 -0500

    make the highscoreplayers after play the quiz

--- app/src/main/java/com/example/piquizapplch/HighScoreActivity.java
+++ app/src/main/java/com/example/piquizapplch/HighScoreActivity.java
@@ -1,6 +1,7 @@
 package com.example.piquizapplch;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HighScoreActivity extends AppCompatActivity {
 
+    TextView firstPlayerTV;
+    TextView firstScorePlayerTV;
+
+    TextView secondPlayerTV;
+    TextView secondScorePlayerTV;
+
+    TextView thirdPlayerTV;
+    TextView thirdScorePlayerTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
+        firstPlayerTV = (TextView)findViewById(R.id.playerName1TV);
+        firstScorePlayerTV = (TextView)findViewById(R.id.playerScore1TV);
+
+        secondPlayerTV = (TextView)findViewById(R.id.playerName2TV);
+        secondScorePlayerTV = (TextView)findViewById(R.id.playerScore2TV);
+
+        thirdPlayerTV = (TextView)findViewById(R.id.playerName3TV);
+        thirdScorePlayerTV = (TextView)findViewById(R.id.playerScore3TV);
+
 
     }
 }
